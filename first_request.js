a6a79f0e4bed86195b0f0a74884b816db07bdd09
laiamanda <40805270+laiamanda@users.noreply.github.com>
2021-03-02 13:45:47 -0500

    Create first_request.js

---
+++ first_request.js
@@ -0,0 +1,31 @@
+var request = require('request');
+/*
+request('http://www.google.com', function (error, response, body) {
+  console.error('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
+
+var request = require ("request");
+
+request ('http://www.google.com',function (error,response,body){
+	if (error){
+		console.log("something went wrong");
+		console.log(error);
+	}
+	else{
+		if (response.statusCode==200){
+			console.log(body);
+		}
+	}
+});
+*/
+
+request('https://jsonplaceholder.typicode.com/users/1',function(error,response,body){
+	
+	//eval(require('locus'));
+	if(!erro && response.status== 200){
+		var parsedData=JSON.parse(body);
+		console.log(parsedData.name);
+	}
+});
